2cd5cd6df7d713f0b0f659420b0c9e461bd421fc
nabute <nabute925@gmail.com>
2020-09-22 03:50:21 +0300

    first commit

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="vjs-custom-skin">
+    <video ref="videoPlayer" class="video-js"></video>
+  </div>
+</template>
+
+<script>
+import videojs from "video.js";
+export default {
+  name: "VideoPlayer",
+  props: {
+    options: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+  data() {
+    return {
+      player: null,
+    };
+  },
+  mounted() {
+    this.player = videojs(
+      this.$refs.videoPlayer,
+      this.options,
+      function onPlayerReady() {
+        console.log("onPlayerReady", this);
+      }
+    );
+  },
+  beforeDestroy() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  },
+};
+</script>
+
+
+<style>
+.vjs-custom-skin {
+  width: 100%;
+  color: #520202 !important;
+}
+
+.vjs-custom-skin > .video-js {
+  width: 100%;
+  min-height: 670px;
+  font-family: "PingFang SC", "Helvetica Neue", "Hiragino Sans GB", "Segoe UI",
+    "Microsoft YaHei", "微软雅黑", sans-serif;
+}
+
+.vjs-custom-skin > .video-js .vjs-play-progress,
+.vjs-custom-skin > .video-js .vjs-volume-level {
+  background-color: #520202 !important;
+}
+
+.vjs-custom-skin > .video-js .vjs-loading-spinner {
+  border-color: #520202 !important;
+}
+
+.video-js:hover .vjs-big-play-button,
+.vjs-custom-skin > .video-js .vjs-big-play-button:focus,
+.vjs-custom-skin > .video-js .vjs-big-play-button:active {
+  background-color: #520202 !important;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div>
+      <video-player :options="videoOptions" />
+    </div>
+  </div>
+</template>
+
+<script>
+import VideoPlayer from "@/components/VideoPlayer.vue";
+export default {
+  components: {
+    VideoPlayer,
+  },
+  data() {
+    return {
+      videoOptions: {
+        autoplay: true,
+        controls: true,
+        sources: [
+          {
+            src:
+              "https://stream.mux.com/VFfLJN8cFMvKAihpIjQhMt00bJtZoZiOO.m3u8",
+            type: "application/x-mpegURL",
+          },
+        ],
+      },
+    };
+  },
+  mounted() {
+    console.log("this is current player instance object", this.myVideoPlayer);
+  },
+  methods: {
+    // listen event
+    onPlayerPlay(player) {
+      // console.log('player play!', player)
+    },
+    onPlayerPause(player) {
+      // console.log('player pause!', player)
+    },
+    onPlayerEnded(player) {
+      // console.log('player ended!', player)
+    },
+    onPlayerLoadeddata(player) {
+      // console.log('player Loadeddata!', player)
+    },
+    onPlayerWaiting(player) {
+      // console.log('player Waiting!', player)
+    },
+    onPlayerPlaying(player) {
+      // console.log('player Playing!', player)
+    },
+    onPlayerTimeupdate(player) {
+      // console.log('player Timeupdate!', player.currentTime())
+    },
+    onPlayerCanplay(player) {
+      // console.log('player Canplay!', player)
+    },
+    onPlayerCanplaythrough(player) {
+      // console.log('player Canplaythrough!', player)
+    },
+    // or listen state event
+    playerStateChanged(playerCurrentState) {
+      console.log("player current update state", playerCurrentState);
+    },
+    // player is ready
+    playerReadied(player) {
+      console.log("example 01: the player is readied", player);
+    },
+  },
+};
+</script>
